09c914e7865ae5236bcac67841ab587ba9d92e29
yashashvisri <138010917+yashashvisri@users.noreply.github.com>
2026-06-22 23:46:43 +0530

    Working with fisrt api development

--- app/api/users.py
+++ app/api/users.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.database.models import User
+from app.schemas.user import UserCreate
 
 router = APIRouter()
 
@@ -6,3 +11,26 @@ router = APIRouter()
 @router.get("/")
 def get_users():
     return {"message": "Users API Working"}
+
+
+@router.post("/")
+def create_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    
+    new_user = User(
+        name=user.name,
+        email=user.email
+    )
+
+    db.add(new_user)
+
+    db.commit()
+
+    db.refresh(new_user)
+
+    return {
+        "message": "User created successfully",
+        "id": new_user.id
+    }
--- app/database/database.py
+++ app/database/database.py
@@ -10,3 +10,12 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
