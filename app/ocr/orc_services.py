68bb8d16991a6e1de44ddc8832192bd45f3d9e63
yashashvisri <yashashvisrivastava93@gmail.com>
2026-06-27 22:52:29 +0530

    Update ocr_services

--- app/ocr/orc_services.py
+++ app/ocr/orc_services.py
@@ -1,8 +1,16 @@
 import pytesseract
 from PIL import Image
 
+# Path to Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+)
+
 
 def extract_text_from_image(image_path: str):
+    """
+    Extract text from an image using OCR.
+    """
 
     image = Image.open(image_path)
 
