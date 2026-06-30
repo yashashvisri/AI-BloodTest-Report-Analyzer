186360da707d828ad7e6bfdcebc158db06e1bd3e
yashashvisri <yashashvisrivastava93@gmail.com>
2026-06-30 17:31:26 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.users import router as users_router
 from app.api.reports import router as reports_router
 
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     users_router,
