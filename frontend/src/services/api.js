ec476f2e870316c12f5dd56c19810e29aa64236a
yashashvisri <yashashvisrivastava93@gmail.com>
2026-09-02 20:44:17 +0530

    feat(core): configure Axios interceptor to attach JWT automatically

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
