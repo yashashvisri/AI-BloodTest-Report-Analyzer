562f346f25bc5e49ecfdef073ecf2f5bf0a9e77e
yashashvisri <138010917+yashashvisri@users.noreply.github.com>
2026-09-26 13:10:28 +0530

    feat(ui): integrate ThemeProvider into application root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
 
     <BrowserRouter>
+    <ThemeProvider>
     <AuthProvider>
 
       <App />
 
         </AuthProvider>
+    </ThemeProvider>
   </BrowserRouter>
 
   </React.StrictMode>
